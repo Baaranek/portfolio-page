33e8172404c5f1895ca48ffdc96366220774f76c
Krystian Otto <otto.krystiann@gmail.com>
2020-10-25 15:02:21 +0100

    add routing in Menu.js, also replace icons with new ones

--- src/components/layout/Menu/Menu.js
+++ src/components/layout/Menu/Menu.js
@@ -1,30 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faAd } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faGraduationCap, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { NavLink } from 'react-router-dom';
 
 
 const Wrapper = styled.section`
-  font-size: 1.3rem;
-  width: 5rem;
+  font-size: 1.5rem;
+  width: 4rem;
   height: 100vh;
   background-color: #181d1f;
-  color: #38e89f;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+
+  > div{
+    padding: 2rem 0;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0 0;
+    text-align: center;
+  }
 `;
 
 const StyledList = styled.li`
-  &:hover{ // Hover będzie z timeOutem wpływał na łądną animacje
-    color: red;
+  padding: .2rem 0;
+`;
+
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  color: #38e89f;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  color: #38e89f;
+  text-decoration: none;
+
+  &.hover {
+    opacity: 0.2;
   }
 `;
 
 const array = [
-  {id:1, icon: faCoffee, description: 'Cofee'},
-  {id:2, icon: faAd, description: 'Ad'},
+  {id:1, icon: faHome, description: 'Home', route: '/'},
+  {id:2, icon: faUser, description: 'About', route: '/about'},
+  {id:3, icon: faGraduationCap, description: 'Skills', route: '/skills'},
+  {id:4, icon: faEnvelope, description: 'Contact', route: '/contact'},
 ];
 
 class Menu extends React.Component {
@@ -40,22 +65,22 @@ class Menu extends React.Component {
     let activeIndex = this.state.activeIndex === index ? null : index;
     setTimeout(() => {
       this.setState({activeIndex});
-    }, 300); // SetTimeout odpowiada ze deleya i uzycie tego w płynnej animacji w przyszłosci
+    }, 100); // SetTimeout odpowiada ze deleya i uzycie tego w płynnej animacji w przyszłosci
   }
 
   render() {
     return (
       <Wrapper>
         <div>
-          <p>Logo</p>
+          <StyledNavLink to='/'> <StyledFontAwesomeIcon icon={faHome} /></StyledNavLink>
         </div>
         <div>
           <ul>
-            { array.map( ({id, icon, description}) =>
+            { array.map( ({id, icon, description, route}) =>
               <StyledList key={id} onMouseEnter={this.hoverHandler.bind(this, id)} onMouseLeave={(e) => this.hoverHandler()}>
                 {this.state.activeIndex === id
-                  ?  description
-                  : <FontAwesomeIcon icon={icon} />
+                  ? <StyledNavLink to={route}>{description}</StyledNavLink>
+                  : <StyledNavLink to={route}> <StyledFontAwesomeIcon icon={icon} /></StyledNavLink>
                 }
               </StyledList>
             )}
@@ -63,9 +88,8 @@ class Menu extends React.Component {
         </div>
         <div>
           <ul>
-            <li><FontAwesomeIcon icon={faAd} /></li>
-            <li><FontAwesomeIcon icon={faAd} /></li>
-            <li><FontAwesomeIcon icon={faAd} /></li>
+            <StyledList><a target='_blank' rel='noopener noreferrer' href='https://github.com/Baaranek'><StyledFontAwesomeIcon icon={faGithub} /></a></StyledList>
+            <StyledList><a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/krystian-otto-8b93241a5/'><StyledFontAwesomeIcon icon={faLinkedin} /></a></StyledList>
           </ul>
         </div>
       </Wrapper>
