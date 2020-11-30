f74b2cf1c25089659f67024b56f1dad834ba8145
Krystian Otto <otto.krystiann@gmail.com>
2020-11-30 17:37:43 +0100

    Updates

---
+++ precache-manifest.01570fa6149f841af188ef332c0e46f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c317d5f7d8e7b5de68ba742c02d429b5",
+    "url": "/portfolio-page/index.html"
+  },
+  {
+    "revision": "9feb881a94078f58c8f7",
+    "url": "/portfolio-page/static/js/2.29846d70.chunk.js"
+  },
+  {
+    "revision": "01c1d165037d4ff6734b72e94737a932",
+    "url": "/portfolio-page/static/js/2.29846d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86af18e164e7f0e02323",
+    "url": "/portfolio-page/static/js/main.0ceefad2.chunk.js"
+  },
+  {
+    "revision": "f66323bbbd858ea16c51",
+    "url": "/portfolio-page/static/js/runtime-main.34d83362.js"
+  }
+]);
